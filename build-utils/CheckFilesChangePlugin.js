7bff6a63affa4e0629b9e6aa2342157292374d48
buckleader <jasmin28virdi@gmail.com>
2020-04-06 03:34:02 +0530

    add version update

--- build-utils/CheckFilesChangePlugin.js
+++ build-utils/CheckFilesChangePlugin.js
@@ -2,11 +2,12 @@ var fs = require('fs');
 var archiver = require('archiver');
 var path = require("path");
 const chalk = require('chalk');
-
+const bump = require('json-bump');
+const glob = require('glob');
 
 class CheckFilesChangePlugin {
     constructor(options) {
-        // console.log("options", options)
+        console.log(chalk.bold.greenBright(options.devMode))
     }
     apply(compiler) {
         //create a zip file of the whole directory
@@ -44,6 +45,20 @@ class CheckFilesChangePlugin {
         archive.directory('src/', false);
         
         archive.finalize();
+    }
+    changeVersion() {
+        let prodMode=true, devTestMode= false;
+        if(this.options.devMode)
+            devTestMode=true;
+
+        glob(`${process.cwd()}/src` + '**/*.json', {}, (err, files)=>{
+            console.log(files)
+            if(files.length >1)
+                console.log("manifest.json exits")
+            })
+
+        if(prodMode && devTestMode)
+            bump('src/manifest.json', { minor : 1 })
     }
     
 }
