9781d079a158165e264765e0c068a22c039b71fb
buckleader <jasmin28virdi@gmail.com>
2020-04-05 19:35:50 +0530

    add file zip plugin

--- build-utils/CheckFilesChangePlugin.js
+++ build-utils/CheckFilesChangePlugin.js
@@ -1,18 +1,34 @@
+var fs = require('fs');
+var archiver = require('archiver');
+var path = ""
+
+
 class CheckFilesChangePlugin {
     constructor(options) {
         // console.log("options", options)
     }
     apply(compiler) {
-        compiler.plugin("emit", (compilation, cb)=> {
-
-            compilation.chunks.forEach(element => {
-                console.log("element", element)
-                console.log("chunk module")
-            });
-            // console.log("compilation", compilation.stats, compilation.createHash);
-            cb();
+        //create a zip file of the whole directory
+        compiler.hooks.emit.tapAsync("CheckFilesChangePlugin", (compilation, cb)=> {
+            console.log("compiler", compilation.assets)
+            this.createzipFile(compilation.assets);
         })
+        console.log("__dirname", __dirname)
+        
     }
+    createzipFile(data) {
+        console.log("createzipFile")
+        let output = fs.createWriteStream(data);
+        output.on("data",function(chunk){
+            console.log("New Chunk Received");
+            console.log(chunk);
+        })
+        // let archive = archiver("zip", {
+        //     zlib: {level:9}
+        // })
+        // archive.pipe(output);
+    }
+    
 }
 
 module.exports = CheckFilesChangePlugin;
